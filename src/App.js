5d179c322242d10813bedc0935f7a4c51f506619
Shalini Bajpai <shalinibajpai0310@gmail.com>
2023-04-13 21:07:15 +0530

    Added search monster functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      monsterList:[],
+      searchField:''
+    }
+  }
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => this.setState(() => {
+      return {monsterList: users}
+    }));
+  }
+
+  searchMonster = (event) =>{
+    const searchField = event.target.value;
+    this.setState({
+      searchField
+    })
+  }
+
+  render(){
+    const {searchField} = this.state;
+    const filteredList = this.state.monsterList.filter((list) => (list.name).toLowerCase().includes(searchField.toLowerCase()));
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input type="search" placeholder='Search monsters' onChange={this.searchMonster}/>
+          <div>
+          {filteredList.map((list) => {
+            return(<div key={list.id}>{list.name}</div>)
+          })}
+          </div>
+         
+        </header>
+      </div>)
+  };
 }
 
 export default App;
